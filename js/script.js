bf26fe290e9e0afce6735e2c38855e2e001f3874
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-13 23:05:37 +0100

    Feature: Added animation for the first card.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // script.js
+
+const card = document.querySelector('.card');
+let playing = false;
+
+card.addEventListener('click', function(e) {
+  console.log(e.target);
+  if(playing) return;
+  console.log('hello');
+  playing = true;
+  anime({
+    targets: card,
+    scale: [{value: 1}, {value: 1.0}, {value: 1, delay: 0}],
+    rotateY: {value: '+=180', delay: 100},
+    easing: 'easeInOutSine',
+    duration: 200,
+    complete: function(anime){
+       playing = false;
+    }
+  });
+});
