c2b73ce2994301d8ce5d06bcbd47ea8f4a097714
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-14 21:58:30 +0100

    Feature: Only two cards can be flipped at once.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 // script.js
-
+let count = 0;
 $('.card').on('click', function(e) {
   console.log(e);
   let parent = $(e.target).parent();
-  if ( parent.hasClass('flipped') ) {
-			parent.removeClass('flipped');
-		} else {
-			parent.addClass('flipped');
-		}
+  if ( parent.hasClass('flipped') )
+  {
+		// sparent.removeClass('flipped');
+	}
+  else {
+    if(count === 2)
+    {
+      unflip();
+      count = 0;
+    }
+    parent.addClass('flipped');
+    count++;
+	}
 });
+
+function unflip() {
+  let flippedCards = document.getElementsByClassName('card');
+  for(card of flippedCards)
+  {
+    if($(card).hasClass('flipped')){
+      $(card).removeClass('flipped');
+    }
+  }
+}
