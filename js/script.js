5d162b2a93843ebf24250537b0931c56f0cacbfb
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-14 20:37:03 +0100

    Feature: Any card can be flipped now, added styling for beter UX.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
 // script.js
 
-const card = document.querySelector('.card');
-let playing = false;
-
-card.addEventListener('click', function(e) {
-  console.log(e.target);
-  if(playing) return;
-  console.log('hello');
-  playing = true;
-  anime({
-    targets: card,
-    scale: [{value: 1}, {value: 1.0}, {value: 1, delay: 0}],
-    rotateY: {value: '+=180', delay: 100},
-    easing: 'easeInOutSine',
-    duration: 200,
-    complete: function(anime){
-       playing = false;
-    }
-  });
+$('.card').on('click', function(e) {
+  console.log(e);
+  let parent = $(e.target).parent();
+  if ( parent.hasClass('flipped') ) {
+			parent.removeClass('flipped');
+		} else {
+			parent.addClass('flipped');
+		}
 });
