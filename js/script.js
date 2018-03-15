1ef0d46a7baa819bb26e0cda50ce665008e752c4
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-15 21:22:06 +0100

    Feature: Added FontAwesome to the project, created array of icons.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // script.js
+
+const icons = ["fas fa-bug", "fas fa-bolt",
+                  "fas fa-camera", "fas fa-chess-king",
+                  "far fa-compass", "fas fa-fighter-jet",
+                  "fas fa-gamepad", "fas fa-paw"];
+
+$('.icon').addClass(icons[1]);
+
 let count = 0;
 $('.card').on('click', function(e) {
   console.log(e);
