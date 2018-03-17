6b7f2123bf4dd081df168b58c081b21a91128952
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-17 12:27:11 +0100

    Feature: Improved cards shuffling.

--- js/script.js
+++ js/script.js
@@ -6,26 +6,29 @@ const icons = ["fas fa-bug", "fas fa-bolt",
                   "fas fa-gamepad", "fas fa-paw"];
 
 let cards = $('.icon');
-let cardBacks = [,,,,,,,,,,,,,,,];
-let cardBacksCopy = [,,,,,,,,,,,,,,,];
-for(let j = 0; j < 9; j += 8)
+let cardBacks = [];
+
+for(let j = 0; j <= 8; j+=8)
 {
-  let randomNumber = Math.round(Math.random() * 8);
   for(let i = 0; i < 8; i++)
   {
-    cardBacks[i+j] = icons[(i + randomNumber) % 8];
+    cardBacks[i+j] = icons[i];
   }
-  const iconsCopy = 1;
 }
-for(let i = 0; i < 8; i++)
-{
-  cardBacksCopy[i] = cardBacks[i*2];
-  cardBacksCopy[i+8] = cardBacks[16 - (i*2+1)];
-}
+
+shuffleArray(cardBacks);
 
 for(let i = 0; i < 16; i++)
 {
-  $(cards[i]).addClass(cardBacksCopy[i]);
+  $(cards[i]).addClass(cardBacks[i]);
+}
+
+// Durstenfeld shuffle
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 let count = 0;
