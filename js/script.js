1ec5a161c4adbe46c6ea4cc36b4f5d4de6058b74
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-20 22:04:13 +0100

    Feature: Added end-game screen with animation.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ const icons = ["fas fa-bug", "fas fa-bolt",
                   "far fa-compass", "fas fa-fighter-jet",
                   "fas fa-gamepad", "fas fa-paw"];
 
+const successAnimation = `<svg id="successAnimation" class="animated" xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 70 70">
+                          <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
+                          <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" stroke-width="2" stroke-linecap="round" fill="transparent"/>
+                          <polyline id="successAnimationCheck" stroke="#979797" stroke-width="2" points="23 34 34 43 47 27" fill="transparent"/>
+                          </svg>`;
+
 let cards = $('.icon');
 let cardBacks = [];
 let movesCount = 0;
@@ -61,7 +67,8 @@ $('.card').on('click', function(e) {
 
       countMoves();      
     }    
-	}
+  }
+  setTimeout(checkIfEndGame, 600);
 });
 
 function unflip() {
@@ -100,6 +107,16 @@ function compareCards(flippedCards, areLogosTheSame)
       unflip();
     }, 1100);
   }      
+}
+
+function checkIfEndGame() {
+  if($('.success').length === 16)
+  {
+    $('.end-screen').addClass('end-screen-show');
+    setTimeout(function(){
+      $('.end-screen').append(successAnimation);
+    }, 1000);
+  }
 }
 
 function countMoves() {
@@ -154,7 +171,7 @@ function startTimer() {
   }, 1000);
 }
 
-$('.btn-reset').on('click', function() {
+function resetGame() {
   $('.stats-timer').text(`00:00`);
   startTimer();
   movesCount = 0;
@@ -165,9 +182,15 @@ $('.btn-reset').on('click', function() {
                .removeClass('far')
                .addClass('fas');
   $('.card-back').removeClass('back-success');
+}
+
+$('.btn-reset').on('click', function() {
+  resetGame();
 });
 
 $('.btn-start').on('click', function() {
   $('.start-screen').addClass('start-screen-hide');
-  setTimeout(startTimer, 100);
+  $('.end-screen').removeClass('end-screen-show');
+  resetGame();
 });
+
